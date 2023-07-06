c948d7d3d0f2cb0000947b83356598f270694755
Sagun Karki <sagunk064@gmail.com>
2023-07-05 19:05:11 -0500

    added homepage and 404 page

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template("index.html")
 
+@app.errorhandler(404)
+def error(e):
+    return render_template("404.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
