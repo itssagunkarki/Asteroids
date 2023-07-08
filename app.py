462933bfbef94b922f6ec12cf359a5b29db87fcd
Sagun Karki <sagunk064@gmail.com>
2023-07-08 14:47:41 -0500

    added data loader from NASA api

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import json
+from flask_caching import Cache
+
+from asteroids.asteroid_data import AsteroidData
 
 app = Flask(__name__)
+cache = Cache(app, config={"CACHE_TYPE": "simple"})
+
 
 @app.route("/")
 def home():
@@ -9,6 +15,35 @@ def home():
 @app.errorhandler(404)
 def error(e):
     return render_template("404.html")
+
+@app.route("/asteroids/", methods=["GET", "POST"])
+def asteroids():
+    if request.method == "GET":
+        if all(key in request.args for key in ["start_date", "end_date"]):
+            start_date = request.args.get("start_date")
+            end_date = request.args.get("end_date")
+
+            data = {
+                "start_date": start_date,
+                "end_date": end_date
+            }
+
+            cached_result = cache.get(f"{start_date}_{end_date}")
+            if cached_result:
+                data['asteroid_count'] = cached_result['asteroid_count']
+                data['asteroids'] = cached_result['asteroids']
+                return render_template("asteroids.html", title=f"Asteroids from {start_date} to {end_date}", data=data)
+
+            asteroids = AsteroidData(start_date, end_date)
+            data['asteroids'] = asteroids.get_list_all_asteroids()
+            data['asteroid_count'] = asteroids.get_num_asteroids()
+
+            cache.set(f"{start_date}_{end_date}", data, timeout=3600)  # Cache the result for 1 hour
+
+            return render_template("asteroids.html", title=f"Asteroids from {start_date} to {end_date}", data=data)
+
+    return render_template("asteroids.html", title="Asteroids")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- asteroids/asteroid_data.py
+++ asteroids/asteroid_data.py
@@ -1,53 +1,80 @@
-"""
-    AsteroidData
-"""
 import os
 import json
 import requests
 from dotenv import load_dotenv
+import pandas as pd
+from pathlib import Path
+from typing import Optional, List
+
 load_dotenv()
 
-class AsteroidData():
-    """
-    returns asteroid data from NASA or predownloaded data
-    """
-    def __init__(self, start_date='0000-00-00', end_date='0000-00-00') -> None:
+class AsteroidData:
+    def __init__(self, start_date: str = '0000-00-00', end_date: str = '0000-00-00', nasa_api_key: Optional[str] = None) -> None:
         self.start_date = start_date
         self.end_date = end_date
+        self.asteroid_data = None
+        self.nasa_api_key = nasa_api_key or os.environ.get('NASA_API_KEY')
 
-    def get_data_from_nasa(self, save_to_file: bool = False) -> json:
-        """
-        Gets data of all asteroids near Earth between `start_date` and `end_date`
-        """
+    def get_data_from_nasa(self, save_to_file: bool = False) -> dict:
+        url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={self.start_date}&end_date={self.end_date}&api_key={self.nasa_api_key}"
 
-        nasa_api_key = os.environ['NASA_API_KEY']
+        with requests.Session() as session:
+            response = session.get(url, timeout=1000)
+            response.raise_for_status()
 
-        response = requests.get(
-            url=f"https://api.nasa.gov/neo/rest/v1/feed?start_date={self.start_date}&end_date={self.end_date}&api_key={nasa_api_key}",
-            timeout=1000
-            )
-        
-        data = response.json()
-        data['links'] = None #Avoid exposing API Keys
+            data = response.json()
+            data['links'] = None  # Avoid exposing API Keys
 
         if save_to_file:
-            self._save_to_json("../assets/json/nasa_json.json", data)
+            json_datafile = self._get_data_file_path()
+            self._save_to_json(json_datafile, data)
 
+        self.asteroid_data = data
         return data
-    
 
-    def _save_to_json(self, json_datafile: str, json_data: json):
-        #  Write the JSON data to the file
-        with open(json_datafile, 'w', encoding="UTF8") as json_file:
+    def _save_to_json(self, json_datafile: Path, json_data: dict) -> None:
+        with json_datafile.open('w', encoding="UTF8") as json_file:
             json.dump(json_data, json_file, indent=4)
 
-
-    def _get_local_data(self, filepath: str) -> json:
-        """
-        returns pre downloaded json data that was created in previous run of the program
-        """
-        with open(filepath, 'r', encoding="UTF8") as file:
+    def _get_local_data(self, filepath: Path) -> dict:
+        with filepath.open('r', encoding="UTF8") as file:
             data = json.load(file)
 
         return data
+
+    def get_list_all_asteroids(self) -> List[dict]:
+        if self.asteroid_data is None:
+            self.get_data_from_nasa()
+
+        asteroids = []
+        data = self.asteroid_data
+
+        for date_key in data['near_earth_objects'].keys():
+            for asteroid_data in data['near_earth_objects'][date_key]:
+                asteroid = {
+                    "Neo Reference ID": asteroid_data['neo_reference_id'],
+                    "Name": asteroid_data['name'],
+                    "Potentially Hazardous": asteroid_data['is_potentially_hazardous_asteroid'],
+                    "Close Approach Date": asteroid_data['close_approach_data'][0]['close_approach_date_full'],
+                    "Sentry Object": asteroid_data['is_sentry_object']
+                }
+                asteroids.append(asteroid)
+
+        df = pd.DataFrame(asteroids)
+        json_data = df.to_dict(orient='records')
+        return json_data
+
+    def get_num_asteroids(self) -> int:
+        if self.asteroid_data is None:
+            self.get_data_from_nasa()
+
+        data = self.asteroid_data
+        return data['element_count']
+
+    def _get_data_file_path(self) -> Path:
+        file_dir = Path(__file__).resolve().parent
+        data_dir = file_dir / 'assets' / 'json'
+        data_dir.mkdir(parents=True, exist_ok=True)
+        return data_dir / 'nasa_json.json'
     
+
